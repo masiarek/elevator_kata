7d888b501d932a8b29d78051f79dc94ba79a4cbe
Adam Masiarek <masiarek@gmail.com>
2024-10-21 17:07:11 -0400

    main vs master - troubleshooting

--- src/floor.rs
+++ src/floor.rs
@@ -7,4 +7,5 @@ struct Floor {
     // fn number_of_people(floor: Floor){
     // let num_of_people_of_floor = rand::thread_rng().gen_range(1..5);
     // println!("Number of people on floor {}", num_of_people_of_floor)
+    // test main master branch
 }
