fc92fee9efa653b960959f879b1e47834ad9fb4c
Adam Masiarek <masiarek@gmail.com>
2024-10-21 15:09:16 -0400

    rename master to main in RustRover

--- src/floor.rs
+++ src/floor.rs
@@ -1,7 +1,10 @@
 use crate::person::Person;
 
 struct Floor {
-    // folks - persons waiting for the Lift to arrive at the Floor
+    // persons are waiting for the Lift to arrive at the Floor
     // represented as a number in a vector (array?)
     person: Vec<Person>,
+    // fn number_of_people(floor: Floor){
+    // let num_of_people_of_floor = rand::thread_rng().gen_range(1..5);
+    // println!("Number of people on floor {}", num_of_people_of_floor);
 }
